bd895bb675c79e235b3c38dd96646e8592a35bec
Anders Arnholm <anders@arnholm.se>
2009-07-21 07:21:59 +0200

    First working copy

--- src/nu/balp/UserMap.java
+++ src/nu/balp/UserMap.java
@@ -1,10 +1,14 @@
 package nu.balp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -50,14 +54,29 @@ public class UserMap {
 				final URL url = new URL(stringUrl);
 				final URLConnection connection = url.openConnection();
 				connection.setDoOutput(true);
+				if (connection instanceof HttpURLConnection) {
+				    ((HttpURLConnection)connection).setRequestMethod("POST");
+				    System.out.println("HTTP Connection");
+				}
 				System.out.println("connected");
 				final OutputStreamWriter out = new OutputStreamWriter(
 						connection.getOutputStream());
 				String fullMessage = name + " on radio:" + message;
+				String encMessage = URLEncoder.encode(name + " on radio:" + message, "UTF-8");
 				out.write(fullMessage);
 				out.flush();
 				out.close();
 				System.out.println("Sent: \""+ fullMessage + "\"");
+				BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+					    connection.getInputStream()));
+
+				String decodedString;
+
+				while ((decodedString = in.readLine()) != null) {
+				    System.out.println(decodedString);
+				}
+				in.close();
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
